48872e5d453ce83d6d4c7a0d67004e25cc6ec29b
juan carlos macias <juancmaciassalvador@gmail.com>
2026-01-13 08:10:45 +0100

    Configure GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/eu-wallet-verifier">
+    <BrowserRouter basename="/eu-wallet-verifier-poc">
       <App />
     </BrowserRouter>
   </React.StrictMode>
